b862ad516a781923b473b4938567729dc6a4a60b
Guhan M <guhan76guhan@gmail.com>
2026-03-22 00:22:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return <>
+  <Dashboard/>
+  {/* <h2>Hello user</h2> */}
+  </>
+}
+
+export default App
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopBar from './Topbar'
+
+function Dashboard() {
+  return <>
+  <TopBar/>
+  </>
+}
+
+export default Dashboard
